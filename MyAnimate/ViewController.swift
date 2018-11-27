e0e7c03f17b2790bfe4dfbbbb96234744efbf805
范云翼 <fanyunyimac@fanyunyimacdemac.local>
2018-11-27 17:30:16 +0800

    最新版，增加了注释对比

--- MyAnimate/ViewController.swift
+++ MyAnimate/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     //增加一个view,与buttons分离
     @IBOutlet weak var backView: UIView!
+   
     @IBOutlet weak var MyView: UIView!
-    
     //在backView上创建一个UIDynamicAnimator
     lazy var animator = UIDynamicAnimator(referenceView: self.backView)
     //创建一个重力行为
@@ -45,41 +45,46 @@ class ViewController: UIViewController {
     }
     //旋转
     @IBAction func MyTrans(_ sender: Any) {
-         UIView.animate(withDuration: 1) {
+         UIView.animate(withDuration: 4) {
             //旋转时放大倍数
             var trans = CGAffineTransform(scaleX: 1, y: 1)
             //旋转时的角度(180度)
             trans = trans.rotated(by: CGFloat(Double.pi))
             //旋转的an affine transformation matrix
-            trans = trans.translatedBy(x: 300, y: 300)
+            //trans = trans.translatedBy(x: 300, y: 300)
             self.MyView.transform = trans
+            //这样也能旋转回来(复原)
+            self.MyView.transform = CGAffineTransform.identity
         }
         //另外180度旋转回来
-        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseInOut, animations: {
-            self.MyView.transform =  CGAffineTransform(rotationAngle: CGFloat(Double.pi))
-        }, completion: { _ in
-            self.MyView.transform =  CGAffineTransform(rotationAngle: CGFloat(0))
-        })
+//        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseInOut, animations: {
+//            self.MyView.transform =  CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//        }, completion: { _ in
+//            self.MyView.transform =  CGAffineTransform(rotationAngle: CGFloat(0))
+//        })
     }
     //视图间的切换
     @IBAction func MyTransition(_ sender: Any) {
-        //增加的子视图
-        let theView = UIView(frame: MyView.bounds)
+        //增加的子视图(从一个视图切换到另一个视图，但只在其上一级父容器里切换)
+        let theView = UIView(frame: MyView.frame)
         theView.backgroundColor = UIColor.green
-        UIView.transition(with: MyView, duration: 2, options: .transitionFlipFromLeft, animations: {
-            self.MyView.addSubview(theView)
-        }, completion: {
-            //切换回来
-            _ in  UIView.transition(with: self.MyView, duration: 2, options: .transitionFlipFromRight, animations: {
-                //移除子视图
-                theView.removeFromSuperview()
-            }, completion: nil)
-        })
-    }
+        UIView.transition(from: MyView, to: theView, duration: 2, options: .transitionCurlUp, completion: nil)
+        
+        //在一个view上操作(比如改它的背景颜色等等)
+//        UIView.transition(with: MyView, duration: 2, options: .transitionFlipFromLeft, animations: {
+//            self.MyView.addSubview(theView)
+//        }, completion: {
+//            //切换回来
+//            _ in  UIView.transition(with: self.MyView, duration: 2, options: .transitionFlipFromRight, animations: {
+//                //移除子视图
+//                theView.removeFromSuperview()
+//            }, completion: nil)
+//        })
+}
     
     //Dynamic(增加视图)
     @IBAction func addView(_ sender: Any) {
-        MyView.isHidden = true
+        self.MyView!.isHidden = true
         let width = Int(backView.bounds.width/10)
         let randX = Int(arc4random() % 10) * width
         let label = UILabel(frame: CGRect(x: randX, y: 5, width: width, height: 2*width))
@@ -118,6 +123,7 @@ class ViewController: UIViewController {
             }
         }
         MyView.isHidden = false
+        gravity.gravityDirection = CGVector(dx: 0, dy: 1)
     }
     
     
